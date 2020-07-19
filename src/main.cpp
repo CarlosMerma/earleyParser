506e81e6c400699755f6e23ffe26b1b78c5a8580
jeanpaulmc <44785966+jeanpaulmc@users.noreply.github.com>
2020-07-19 15:20:33 -0500

    ep

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Familiar on 19/07/2020.
+//
+
+
+#include "earley.h"
+#include "grammar.h"
+
+int main()
+{
+    std::string filename = "earley.dat";
+    std::ifstream ifs(filename);
+    if (ifs) {
+        MB::grammar grammar(ifs);
+        std::string sentence[] = {"John", "called", "Mary", "from", "Denver"};
+        const size_t len = sizeof(sentence) / sizeof(sentence[0]);
+        bool success = MB::earley_parser(grammar).parse(sentence, sentence + len, std::cout);
+        std::cout << "Success: " << std::boolalpha << success << '\n';
+    }
+    else {
+        std::cerr << "Couldn't open " << filename << " for reading\n";
+    }
+}
